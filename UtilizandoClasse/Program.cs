be1c5963440e71bdf166c56595279ab9d090060e
Henrique Melo <Henrique-125@hotmail.com>
2022-07-19 19:04:57 -0300

    Escolha de Gênero de Filme utilizando Class

---
+++ UtilizandoClasse/Program.cs
@@ -0,0 +1,26 @@
+﻿using CriandoClasse.src;
+using System;
+
+namespace CriandoClasse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Filmes Henrique = new Filmes(Gênero.Ficção, "Perdido em Marte", 10);
+            Filmes Jéssica = new Filmes(Gênero.Suspense, "Um contra Tempo", 9.2);
+            Filmes Jorge = new Filmes(Gênero.Ação, "Vingadores", 8.7);
+            Filmes Larissa = new Filmes(Gênero.Aventura, "Homem Aranha", 8.7);
+
+            Console.WriteLine(Henrique.ToString());
+            Console.WriteLine(Jéssica.ToString());
+            Console.WriteLine(Jorge.ToString());
+            Console.WriteLine(Larissa.ToString());
+            
+
+            Filmes Carlos = new Filmes(Gênero.Romance);
+
+            Console.WriteLine(Carlos);
+        }
+    }
+}
---
+++ UtilizandoClasse/src/Filmes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoClasse.src
+{
+    public class Filmes
+    {
+        public Gênero Gênero { get; set; }
+        public string NomeDoFilme { get; set; }
+        public double NotaDoFilme { get; set; }
+
+        public Filmes(Gênero gênero)
+        {
+            Gênero = gênero;
+        }
+
+        public Filmes(Gênero gênero, string nomeDoFilme, double notaDoFilme)
+        {
+            Gênero = gênero;
+            NomeDoFilme = nomeDoFilme;
+            NotaDoFilme = notaDoFilme;
+        }
+
+        public override string ToString()
+        {
+            return 
+                $"\n O gênero de filme que mais gosto é: {Gênero} " +
+                $"\n Nome do Filme é: {NomeDoFilme} " +
+                $"\n Minha nota para esse filme: {NotaDoFilme}";
+        }
+    }
+    public enum Gênero
+    {
+        Ação, 
+        Suspense,
+        Ficção,
+        Aventura,
+        Romance
+    }
+}
